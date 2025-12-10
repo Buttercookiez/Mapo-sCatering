e2c829c31acee906ec7a22f57c5ee5f41f2162b9
Buttercookiez <johnfrancismarabe2005@gmail.com>
2025-12-10 22:21:41 +0800

    Update: Fix Dark Mode and Light Mode

--- MaposAdmin/tailwind.config.js
+++ MaposAdmin/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx,html}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  // CHANGE THIS LINE:
+  darkMode: 'class', // <--- This allows you to toggle it manually
   theme: {
     extend: {},
   },
